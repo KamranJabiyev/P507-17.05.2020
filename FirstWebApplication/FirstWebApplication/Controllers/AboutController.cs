577164ade48f4f083cda16e04cc9a9ca56b25e33
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2020-05-17 19:19:21 +0400

    Add files via upload

---
+++ FirstWebApplication/FirstWebApplication/Controllers/AboutController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApplication.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+            //return Json(new { 
+            //    Id=1,
+            //    Name="Mubush",
+            //    Surname="Memmedzade"
+            //});
+
+            //return File("~/images/ayi.jpg", "image/jpg");
+        }
+    }
+}
---
+++ FirstWebApplication/FirstWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWebApplication.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+            //ViewResult view = new ViewResult();
+            //view.ViewName = "Index";
+            //return view;
+        }
+
+        public ViewResult Test()
+        {
+            return View();
+        }
+    }
+}
